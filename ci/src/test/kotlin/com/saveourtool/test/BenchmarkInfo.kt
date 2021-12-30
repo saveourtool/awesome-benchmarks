6e0c256e7f7c4f20fb16755b357c75f18f4ba110
Andrey Kuleshov <andrewkuleshov7@gmail.com>
2021-12-30 18:03:14 +0300

    Validation

---
+++ ci/src/test/kotlin/com/saveourtool/test/BenchmarkInfo.kt
@@ -0,0 +1,32 @@
+package com.saveourtool.test
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class BenchmarkInfo(
+    val general: General,
+    val info: Info,
+    val links: Links,
+)
+
+@Serializable
+data class General(
+    val name: String,
+    val category: BenchmarkCategoryEnum,
+    val tags: ArrayList<String>,
+)
+
+@Serializable
+data class Info(
+    val language: String,
+    val license: String,
+    val scenarios_num: Long,
+    val description: String,
+)
+
+@Serializable
+data class Links(
+    val homepage: String,
+    val sources: String,
+    val documentation: String,
+)
